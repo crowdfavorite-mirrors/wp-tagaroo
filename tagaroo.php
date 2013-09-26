crowdfavorite-mirrors/wp-tagaroo

commit 37adb4a54e44d91f980468e8731c6627bd3e8dc7

Added filter for custom post type support.

tagaroo.php
@@ -829,8 +829,11 @@ function oc_admin_head() {
 	<![endif]-->
 	');
 		if (OC_WP_GTE_27) {
-			add_meta_box('oc_tag_controls', 'tagaroo Tags', 'oc_render_tag_controls', 'post', 'normal', 'high');
-			add_meta_box('oc_image_controls', 'tagaroo Images', 'oc_render_image_controls', 'post', 'normal', 'high');
+			$tagaroo_post_types = apply_filters('oc_tagaroo_post_types', array('post'));
+			foreach ($tagaroo_post_types as $post_type) {
+				add_meta_box('oc_tag_controls', 'tagaroo Tags', 'oc_render_tag_controls', $post_type, 'normal', 'high');
+				add_meta_box('oc_image_controls', 'tagaroo Images', 'oc_render_image_controls', $post_type, 'normal', 'high');
+			}
 		}
 	}
 }
@@ -879,7 +882,7 @@ function oc_save_post($post_id, $post) {
 		return;
 	}
 	if ($post->post_status == 'publish') {
-		// commit the content to opencalais
+		// commit the content to opencalais1
 		$privacy_prefs = get_option('oc_privacy_prefs');
 
 		$oc_id = get_post_meta($post_id, 'oc_commit_id');

commit 9ef77e1d4fb3d67598e0a6b5d01d12691f570493

fixing typo introduced in 37adb4a54e44d91f980468e8731c6627bd3e8dc7

tagaroo.php
@@ -882,7 +882,7 @@ function oc_save_post($post_id, $post) {
 		return;
 	}
 	if ($post->post_status == 'publish') {
-		// commit the content to opencalais1
+		// commit the content to opencalais
 		$privacy_prefs = get_option('oc_privacy_prefs');
 
 		$oc_id = get_post_meta($post_id, 'oc_commit_id');